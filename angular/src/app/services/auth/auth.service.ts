87ffd1cad1cfc47fa1af261c4ce9b268c004fe35
Oak Kyaw <oak.kyaw@nintex.com>
2018-09-29 22:38:13 +0800

    angular connects with node auth login api

---
+++ angular/src/app/services/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+// import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { HttpService } from '../apis/http.service';
+import { ServerResponseCodes } from '../../models';
+ 
+@Injectable()
+export class AuthService {
+    constructor(private http: HttpService<any>) {
+    }
+   
+    public login(email: string, password: string): Observable<any> {
+        return this.http.post('/auth/login', { email: email, password: password })
+            .map((result: any) => {
+                if (result.status === ServerResponseCodes.SUCCESS) {
+                    if (result.data) {
+                        return result.data;
+                    } 
+                    return null;
+                }
+                return null;
+            }).catch(err => Observable.throw(err));
+    }
+
+    public logout(): void {
+    }
+}
--- angular/src/app/services/index.ts
+++ angular/src/app/services/index.ts
@@ -1,2 +1,9 @@
 export { ValidatorService } from './validator.service';
 export { LocationService } from './location.service';
+
+// Auth Services
+export { AuthService } from './auth/auth.service';
+
+// Api Services
+export { HttpService } from './apis/http.service';
+export { UserApiService } from './apis/user-api.service';
